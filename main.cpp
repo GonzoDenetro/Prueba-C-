5b930bee4702b8414a46ae0562f438a9977cf56e
Gonzo Denetro <gonzi.ceron.denetro@gmailcom>
2021-11-14 13:50:37 -0600

    Movimientos listos

--- main.cpp
+++ main.cpp
@@ -14,8 +14,22 @@ void figura(Coordenadas Figurin){
 	rectangulo_lleno(Figurin.x1, Figurin.y1, Figurin.x1 + 10, Figurin.y1 + 10);
 }
 
-void moverFigura(){
+void Malo(Coordenadas Villano){
+	color_rgb(249, 65, 68);
+	rectangulo_lleno(Villano.x1, Villano.y1, Villano.x1 + 10, Villano.y1 + 10);
+}
+/*void moverFigura(){
 	
+}*/
+
+void pintarVentana(){
+	vredimensiona(1000, 600); //Dimensionar ventana
+	color_rgb(3, 4, 94);
+	rectangulo_lleno(0, 0, 1000, 600);
+}
+void pintarMalos(){
+	Coordenadas Villano = {140, 85};
+	Malo(Villano);
 }
 
 int main(){
@@ -29,6 +43,9 @@ int main(){
 	Coordenadas Figurin = {10, 40}; //iNICIA X 10 y termina x 20, y en Y empieza 40 y termina 50
 	figura(Figurin);
 	
+	Coordenadas Villano = {150, 90};
+	Malo(Villano);
+	
 	/* color_rgb(0, 180, 216);
 	circulo_lleno(x, y, 20); */
 	
@@ -39,27 +56,74 @@ int main(){
 		tecla_actual = tecla();
 		
 		if(tecla_actual == int(DERECHA)){
-			/*x = x + 10;
-			color_rgb(0, 180, 216);
-			circulo_lleno(x, y, 20);
-			tecla_actual = tecla();*/
+			if(Figurin.x1 >= 980){
+				Figurin.x1 = 980;
+			}
+			else {			
+				Figurin.x1 = Figurin.x1 + 15;
+				borra();
+				pintarVentana();
+				pintarMalos();
+				figura(Figurin);
+				tecla_actual = tecla();
+			}	
+		}
+		else if(tecla_actual == int(IZQUIERDA)){
+			if(Figurin.x1 <= 20){
+				Figurin.x1 = 20;
+			} 
+			else {
+				Figurin.x1 = Figurin.x1 - 15;
 			
-			Figurin.x1 = Figurin.x1 + 15;
 			borra();
-			vredimensiona(1000, 600); //Dimensionar ventana
-			color_rgb(3, 4, 94);
-			rectangulo_lleno(0, 0, 1000, 600);
+			pintarVentana();
+			pintarMalos();
+			
+			figura(Figurin);
+			tecla_actual = tecla();
+			}
+		}
+		else if(tecla_actual == int(ARRIBA))
+		{
+			if(Figurin.y1 <= 10){
+				Figurin.y1 = 10;
+			} 
+			else {
+				Figurin.y1 = Figurin.y1 - 15;
+			
+			borra();
+			pintarVentana();
+			pintarMalos();
+			figura(Figurin);
+			tecla_actual = tecla();
+			}
+		}
+		else if(tecla_actual == int(ABAJO)){
+			Figurin.y1 = Figurin.y1 + 15;
+			
+			borra();
+			pintarVentana();
+			pintarMalos();
+			
 			figura(Figurin);
 			tecla_actual = tecla();
 		}
-		else if(tecla_actual == int(IZQUIERDA)){
-			Figurin.x1 = Figurin.x1 - 15;
+		
+		if(Figurin.x1 + 12 >= Villano.x1 && Figurin.y1 <= Villano.y1){
+			mensaje("Perdiste");
+			Figurin.x1 = 10;
+			Figurin.y1 = 40;
+			borra();
+			pintarVentana();
+			pintarMalos();
+			
 			figura(Figurin);
 			tecla_actual = tecla();
 		}
 		
 		refresca();
 		tecla_actual = tecla();
-		espera(100);	
+		espera(100);
+			
 	}
 }
